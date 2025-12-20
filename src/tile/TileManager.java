9aded2fd7b0be792d936a140d13649e1deee5c64
elijahgott <gottelijah@gmail.com>
2025-12-20 13:31:20 -0600

    created many new tiles

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -21,7 +21,7 @@ public class TileManager {
     public TileManager(GamePanel gp) {
         this.gp = gp;
 
-        tile = new Tile[10]; // 10 types of tiles - grass, dirt, etc
+        tile = new Tile[100]; // 100 different types of tiles
         mapTileNum = new int[gp.maxWorldCol][gp.maxWorldRow];
 
         getTileImage();
@@ -29,12 +29,127 @@ public class TileManager {
     }
 
     public void getTileImage(){
-        setup(0, "grass", false);
-        setup(1, "earth", false);
-        setup(2, "sand", false);
-        setup(3, "water", true);
-        setup(4, "tree", true);
-        setup(5, "wall", true);
+        // grass tiles
+        setup(0, "grass/grass_00", false);
+        setup(1, "grass/grass_01", false);
+        setup(2, "grass/grass_02", false);
+
+        setup(3, "grass/grass_left", false);
+        setup(4, "grass/grass_right", false);
+
+        setup(5, "grass/grass_bottomLeft", false);
+        setup(6, "grass/grass_bottom", false);
+        setup(7, "grass/grass_bottomRight", false);
+
+        setup(8, "grass/grass_topLeft", false);
+        setup(9, "grass/grass_top", false);
+        setup(10, "grass/grass_topRight", false);
+
+        // i may need these at some point
+        // using grass_00 as placeholder for grass_inside_topLeft
+        setup(11, "grass/grass_00", false); // grass_inside_topLeft
+        setup(12, "grass/grass_00", false); // grass_inside_topRight
+        setup(13, "grass/grass_00", false); // grass_inside_bottomLeft
+        setup(14, "grass/grass_00", false); // grass_inside_bottomRight
+
+        // grass-sand tiles
+        setup(15, "grass-sand/grass_sand_left", false);
+        setup(16, "grass-sand/grass_sand_right", false);
+
+        setup(17, "grass-sand/grass_sand_bottomLeft", false);
+        setup(18, "grass-sand/grass_sand_bottom", false);
+        setup(19, "grass-sand/grass_sand_bottomRight", false);
+
+        setup(20, "grass-sand/grass_sand_topLeft", false);
+        setup(21, "grass-sand/grass_sand_top", false);
+        setup(22, "grass-sand/grass_sand_topRight", false);
+
+        setup(23, "grass-sand/grass_sand_inside_topLeft", false);
+        setup(24, "grass-sand/grass_sand_inside_topRight", false);
+        setup(25, "grass-sand/grass_sand_inside_bottomLeft", false);
+        setup(26, "grass-sand/grass_sand_inside_bottomRight", false);
+
+        // path tiles
+        setup(27, "path/path_00", false);
+        setup(28, "path/path_01", false);
+        setup(29, "path/path_02", false);
+
+        setup(30, "path/path_left", false);
+        setup(31, "path/path_right", false);
+
+        setup(32, "path/path_bottomLeft", false);
+        setup(33, "path/path_bottom", false);
+        setup(34, "path/path_bottomRight", false);
+
+        setup(35, "path/path_topLeft", false);
+        setup(36, "path/path_top", false);
+        setup(37, "path/path_topRight", false);
+
+        setup(38, "path/path_inside_topLeft", false);
+        setup(39, "path/path_inside_topRight", false);
+        setup(40, "path/path_inside_bottomLeft", false);
+        setup(41, "path/path_inside_bottomRight", false);
+
+        // sand tiles
+        setup(42, "sand/sand_00", false);
+        setup(43, "sand/sand_01", false);
+
+        setup(44, "sand/sand_left", false);
+        setup(45, "sand/sand_right", false);
+
+        setup(46, "sand/sand_bottomLeft", false);
+        setup(47, "sand/sand_bottom", false);
+        setup(48, "sand/sand_bottomRight", false);
+
+        setup(49, "sand/sand_topLeft", false);
+        setup(50, "sand/sand_top", false);
+        setup(51, "sand/sand_topRight", false);
+
+        setup(52, "sand/sand_inside_topLeft", false);
+        setup(53, "sand/sand_inside_topRight", false);
+        setup(54, "sand/sand_inside_bottomLeft", false);
+        setup(55, "sand/sand_inside_bottomRight", false);
+
+        // water tiles
+        setup(56, "water/water_00", true);
+        setup(57, "water/water_01", true);
+        setup(58, "water/water_02", true);
+
+        setup(59, "water/water_left", true);
+        setup(60, "water/water_right", true);
+
+        setup(61, "water/water_bottomLeft", true);
+        setup(62, "water/water_bottom", true);
+        setup(63, "water/water_bottomRight", true);
+
+        setup(64, "water/water_topLeft", true);
+        setup(65, "water/water_top", true);
+        setup(66, "water/water_topRight", true);
+
+        // do i need inside waters??
+        // using water_00 as placeholder for insides
+        setup(67, "water/water_00", false); // water_inside_topLeft
+        setup(68, "water/water_00", false); // water_inside_topRight
+        setup(69, "water/water_00", false); // water_inside_bottomLeft
+        setup(70, "water/water_00", false); // water_inside_bottomRight
+
+        // tree tiles
+        setup(71, "trees/tree_01", true);
+        setup(72, "trees/tree_02", true);
+
+        // stone brick wall tiles
+        setup(73, "walls/wall_middle", true);
+
+        setup(74, "walls/stone_brick_wall_left", true);
+        setup(75, "walls/stone_brick_wall_right", true);
+
+        setup(76, "walls/stone_brick_wall_bottomLeft", true);
+        setup(77, "walls/stone_brick_wall_bottom", true);
+        setup(78, "walls/stone_brick_wall_bottomRight", true);
+
+        setup(79, "walls/stone_brick_wall_topLeft", true);
+        setup(80, "walls/stone_brick_wall_top", true);
+        setup(81, "walls/stone_brick_wall_topRight", true);
     }
 
     public void setup(int index, String imageName, boolean collision){
